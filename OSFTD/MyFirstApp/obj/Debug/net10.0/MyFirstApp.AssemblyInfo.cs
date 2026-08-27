e9d1cf6167aeeb2445136baaf96312b19d1fe587
weknewtheworldwouldnotbethesame <zerefjose26@gmail.com>
2026-08-27 14:34:44 -0400

    c# variables

--- OSFTD/MyFirstApp/obj/Debug/net10.0/MyFirstApp.AssemblyInfo.cs
+++ OSFTD/MyFirstApp/obj/Debug/net10.0/MyFirstApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyFirstApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48f4d8780affe358de8415524af681c482969360")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+068e8b4e838fb77ffb49e4b9421400adc43b9e1b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyFirstApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyFirstApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
